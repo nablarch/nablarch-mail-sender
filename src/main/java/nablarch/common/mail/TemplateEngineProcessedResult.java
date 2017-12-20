2359a6e292a951110b3f2b9ef0ad88021fc98fa5
Taichi Uragami <backpaper0@gmail.com>
2017-12-20 15:27:32 +0900

    件名と本文はデリミタで分割するようにした

--- src/main/java/nablarch/common/mail/TemplateEngineProcessedResult.java
+++ src/main/java/nablarch/common/mail/TemplateEngineProcessedResult.java
@@ -1,5 +1,8 @@
 package nablarch.common.mail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * テンプレートエンジンで処理した結果を保持するクラス。
  * 
@@ -7,6 +10,8 @@ package nablarch.common.mail;
  *
  */
 public class TemplateEngineProcessedResult {
+
+    public static final String DEFAULT_DELIMITER = "---";
 
     /** 件名 */
     private final String subject;
@@ -33,13 +38,28 @@ public class TemplateEngineProcessedResult {
      * ルールに則って文字列を件名と本文に分割して{@link TemplateEngineProcessedResult}のインスタンスを生成するファクトリーメソッド。
      * 
      * <p>
-     * 文字列に適用される分割のルールは次の通り。
+     * これは{@link #DEFAULT_DELIMITER デフォルトのデリミタ}を指定して{@link #valueOf(String, String)}を呼び出すショートカットである。
+     * </p>
+     * 
+     * @param value 件名と本文を含む文字列
+     * @return 件名と本文がセットされたインスタンス
+     */
+    public static TemplateEngineProcessedResult valueOf(final String value) {
+        return valueOf(value, DEFAULT_DELIMITER);
+    }
+
+    /**
+     * テンプレートエンジンで処理済みの文字列を件名と本文に分割して{@link TemplateEngineProcessedResult}のインスタンスを生成するファクトリーメソッド。
+     * 
+     * <p>
+     * 文字列はデリミタによって件名と本文に分割される。
+     * 基本的なルールは次の通り。
      * </p>
      * 
      * <ol>
-     * <li>1行目は件名とみなす</li>
-     * <li>2行目は無視</li>
-     * <li>3行目以降を本文とみなす</li>
+     * <li>デリミタは引数{@code delimiter}で表される文字列だけからなる行とする（つまり前後に余計な文字を含むものはデリミタとみなさない）</li>
+     * <li>デリミタより前にある行は件名とみなす</li>
+     * <li>デリミタより後の文字列をすべて本文とみなす</li>
      * </ol>
      * 
      * <p>
@@ -47,33 +67,64 @@ public class TemplateEngineProcessedResult {
      * </p>
      * 
      * <pre>{@code テスト件名
-     * 
+     * ---
      * テスト本文１
      * テスト本文２
      * テスト本文３
      * }</pre>
      * 
      * <p>
-     * この場合、{@code テスト件名}が件名となり、{@code テスト本文１}以降が本文となる。
+     * デリミタが{@literal "---"}である場合、{@code テスト件名}が件名となり、{@code テスト本文１}以降が本文となる。
+     * </p>
+     * 
+     * <p>
+     * デリミタよりも前、件名を期待するエリアでは空行は無視される。
+     * つまり、次のような文字列は有効である。
+     * </p>
+     * 
+     * <pre>{@code
+     * テスト件名
+     * 
+     * 
+     * ---
+     * テスト本文１
+     * テスト本文２
+     * テスト本文３
+     * }</pre>
+     * 
+     * <p>
+     * デリミタよりも後、本文を構成するエリアでは空行も無視されずそのまま本文として扱われる。
      * </p>
      * 
      * <p>
      * なお、このファクトリーメソッドでは文字セットは設定されない。
      * </p>
      * 
-     * @param value 件名と本文を含む文字列
+     * @param value 件名とデリミタと本文を含む文字列
+     * @param delimiter 件名と本文を分けるデリミタ
      * @return 件名と本文がセットされたインスタンス
+     * @throws IllegalArgumentException 次のいずれかの場合に投げられる。
+     *          <ul>
+     *          <li>デリミタが含まれない場合</li>
+     *          <li>件名が無い場合</li>
+     *          <li>件名が改行を含んで複数行に渡っている場合</li>
+     *          <li>本文が無い場合</li>
+     *          </ul>
      */
-    public static TemplateEngineProcessedResult valueOf(final String value) {
+    public static TemplateEngineProcessedResult valueOf(final String value,
+            final String delimiter) throws IllegalArgumentException {
         class Tokenizer {
             int index = 0;
 
             public String nextLine() {
+                if (index < value.length() == false) {
+                    return null;
+                }
                 StringBuilder sb = new StringBuilder();
                 while (index < value.length()) {
                     char c = value.charAt(index++);
                     if (c == '\r') {
-                        if (value.charAt(index) == '\n') {
+                        if (index < value.length() && value.charAt(index) == '\n') {
                             index++;
                         }
                         break;
@@ -90,9 +141,30 @@ public class TemplateEngineProcessedResult {
             }
         }
         Tokenizer tokenizer = new Tokenizer();
-        String subject = tokenizer.nextLine();
-        tokenizer.nextLine(); //2行目は読み捨てる
+        String line;
+        List<String> subjectCandidates = new ArrayList<String>();
+        boolean delimiterFound = false;
+        while (null != (line = tokenizer.nextLine())) {
+            if (line.equals(delimiter)) {
+                delimiterFound = true;
+                break;
+            }
+            if (line.isEmpty() == false) {
+                subjectCandidates.add(line);
+            }
+        }
+        if (delimiterFound == false) {
+            throw new IllegalArgumentException("Delimiter(" + delimiter + ") is not found");
+        } else if (subjectCandidates.isEmpty()) {
+            throw new IllegalArgumentException("Subject is not found");
+        } else if (subjectCandidates.size() > 1) {
+            throw new IllegalArgumentException("Subject must not have new line");
+        }
+        String subject = subjectCandidates.get(0);
         String mailBody = tokenizer.remaining();
+        if (mailBody.isEmpty()) {
+            throw new IllegalArgumentException("Mail body is not found");
+        }
         return new TemplateEngineProcessedResult(subject, mailBody, null);
     }
 
--- src/test/java/nablarch/common/mail/TemplateEngineProcessedResultTest.java
+++ src/test/java/nablarch/common/mail/TemplateEngineProcessedResultTest.java
@@ -3,12 +3,17 @@ package nablarch.common.mail;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * {@link TemplateEngineProcessedResult}のテストクラス。
  */
 public class TemplateEngineProcessedResultTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     /**
      * 改行コードがLFの場合にファクトリーメソッドが機能すること。
@@ -16,7 +21,7 @@ public class TemplateEngineProcessedResultTest {
     @Test
     public void testValueOf_LF() {
         TemplateEngineProcessedResult sut = TemplateEngineProcessedResult
-                .valueOf("aaa\n\nbbb\nccc\nddd");
+                .valueOf("aaa\n---\nbbb\nccc\nddd");
         assertThat(sut.getSubject(), is("aaa"));
         assertThat(sut.getMailBody(), is("bbb\nccc\nddd"));
     }
@@ -27,31 +32,64 @@ public class TemplateEngineProcessedResultTest {
     @Test
     public void testValueOf_CRLF() {
         TemplateEngineProcessedResult sut = TemplateEngineProcessedResult
-                .valueOf("aaa\r\n\r\nbbb\r\nccc\r\nddd");
+                .valueOf("aaa\r\n---\r\nbbb\r\nccc\r\nddd");
         assertThat(sut.getSubject(), is("aaa"));
         assertThat(sut.getMailBody(), is("bbb\r\nccc\r\nddd"));
     }
 
     /**
-     * 件名だけの場合にファクトリーメソッドが機能すること。
+     * 改行コードがCRの場合にファクトリーメソッドが機能すること。
      */
     @Test
-    public void testValueOf_subject_only() {
+    public void testValueOf_CR() {
         TemplateEngineProcessedResult sut = TemplateEngineProcessedResult
-                .valueOf("aaa");
+                .valueOf("aaa\r---\rbbb\rccc\rddd");
         assertThat(sut.getSubject(), is("aaa"));
-        assertThat(sut.getMailBody(), is(""));
+        assertThat(sut.getMailBody(), is("bbb\rccc\rddd"));
     }
 
     /**
-     * 本文だけの場合にファクトリーメソッドが機能すること。
+     * 本文が無い場合にファクトリーメソッドが例外を投げること。
      */
     @Test
-    public void testValueOf_body_only() {
-        TemplateEngineProcessedResult sut = TemplateEngineProcessedResult
-                .valueOf("\r\n\r\nbbb\r\nccc\r\nddd");
-        assertThat(sut.getSubject(), is(""));
-        assertThat(sut.getMailBody(), is("bbb\r\nccc\r\nddd"));
+    public void testValueOf_thrown_exception_mailBody_not_found() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Mail body is not found");
+
+        TemplateEngineProcessedResult.valueOf("subject only\r\n---\r\n");
+    }
+
+    /**
+     * 件名が無い場合にファクトリーメソッドが例外を投げること。
+     */
+    @Test
+    public void testValueOf_thrown_exception_subject_not_found() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Subject is not found");
+
+        TemplateEngineProcessedResult.valueOf("\r\n---\r\nbbb\r\nccc\r\nddd");
+    }
+
+    /**
+     * デリミタが無い場合にファクトリーメソッドが例外を投げること。
+     */
+    @Test
+    public void testValueOf_thrown_exception_delimiter_not_found() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Delimiter(---) is not found");
+
+        TemplateEngineProcessedResult.valueOf("aaa\r\n\r\nbbb\r\nccc\r\nddd");
+    }
+
+    /**
+     * 件名が改行を含む場合にファクトリーメソッドが例外を投げること。
+     */
+    @Test
+    public void testValueOf_thrown_exception_subject_break_line() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Subject must not have new line");
+
+        TemplateEngineProcessedResult.valueOf("aaa\r\naaa\r\n---\r\nbbb\r\nccc\r\nddd");
     }
 
     /**
@@ -60,8 +98,52 @@ public class TemplateEngineProcessedResultTest {
     @Test
     public void testValueOf_surrogate_pair() {
         TemplateEngineProcessedResult sut = TemplateEngineProcessedResult
-                .valueOf("aaa\uD867\uDE3D\r\n\r\nbbb\uD867\uDE3D");
+                .valueOf("aaa\uD867\uDE3D\r\n---\r\nbbb\uD867\uDE3D");
         assertThat(sut.getSubject(), is("aaa\uD867\uDE3D"));
         assertThat(sut.getMailBody(), is("bbb\uD867\uDE3D"));
+    }
+
+    /**
+     * 本文にデリミタと同じ文字列の行があってもファクトリーメソッドが機能すること。
+     */
+    @Test
+    public void testValueOf_same_line_as_delimiter() {
+        TemplateEngineProcessedResult sut = TemplateEngineProcessedResult
+                .valueOf("aaa\r\n---\r\nbbb\r\n---\r\nddd");
+        assertThat(sut.getSubject(), is("aaa"));
+        assertThat(sut.getMailBody(), is("bbb\r\n---\r\nddd"));
+    }
+
+    /**
+     * デリミタより前（件名を期待するエリア）では空行は無視されること。
+     */
+    @Test
+    public void testValueOf_ignore_empty_line_in_before_delemiter() {
+        TemplateEngineProcessedResult sut = TemplateEngineProcessedResult
+                .valueOf("\r\naaa\r\n\r\n---\r\nbbb\r\nccc\r\nddd");
+        assertThat(sut.getSubject(), is("aaa"));
+        assertThat(sut.getMailBody(), is("bbb\r\nccc\r\nddd"));
+    }
+
+    /**
+     * デリミタより後（本文のエリア）では空行はそのまま本文に含まれること。
+     */
+    @Test
+    public void testValueOf_contain_empty_line_in_after_delemiter() {
+        TemplateEngineProcessedResult sut = TemplateEngineProcessedResult
+                .valueOf("aaa\r\n---\r\n\r\nbbb\r\n\r\nccc\r\n\r\nddd\r\n");
+        assertThat(sut.getSubject(), is("aaa"));
+        assertThat(sut.getMailBody(), is("\r\nbbb\r\n\r\nccc\r\n\r\nddd\r\n"));
+    }
+
+    /**
+     * ファクトリーメソッドにデリミタを指定できること
+     */
+    @Test
+    public void testValueOf_with_delimiter() {
+        TemplateEngineProcessedResult sut = TemplateEngineProcessedResult
+                .valueOf("aaa\r\n***\r\nbbb\r\nccc\r\nddd", "***");
+        assertThat(sut.getSubject(), is("aaa"));
+        assertThat(sut.getMailBody(), is("bbb\r\nccc\r\nddd"));
     }
 }
